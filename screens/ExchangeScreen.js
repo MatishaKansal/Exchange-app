271481c3596ac125edc0145a71357a0ef9927f1e
MatishaKansal <64193940+MatishaKansal@users.noreply.github.com>
2021-01-24 21:57:44 +0530

    Add files via upload

---
+++ screens/ExchangeScreen.js
@@ -0,0 +1,98 @@
+import * as React from "react";
+import {
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import firebase from "firebase";
+import db from "../config";
+
+export default class ExchangeScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userId: firebase.auth().currentUser.email,
+      Iname: "",
+      Idescription: "",
+    };
+  }
+
+  createUniqueId() {
+    return Math.random().toString(36).substring(7);
+  }
+
+  addItem = (itemName, description) => {
+    var userName = this.setState.userId;
+    var randomRequestId = this.createUniqueId();
+    db.collection("exchange_requests").add({
+      username: userName,
+      item_name: this.state.Iname,
+      exchange_id: randomRequestId,
+      item_description: this.state.Idescription,
+    });
+    this.setState({
+      Iname: "",
+      Idescription: "",
+    });
+
+    return Alert.alert("Item ready to exchange", "", [
+      {
+        text: "OK",
+        onPress: () => {
+          this.props.navigation.navigate("HomeScreen");
+        },
+      },
+    ]);
+  };
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          placeholder="Item Name"
+          onChangeText={(text) =>
+            this.setState({
+              Iname: text,
+            })
+          }
+        ></TextInput>
+        <TextInput
+          placeholder="Item Description"
+          onChangeText={(text) =>
+            this.setState({
+              Idescription: text,
+            })
+          }
+        ></TextInput>
+        <TouchableOpacity
+          style={[styles.button, { marginTop: 10 }]}
+          onPress={() => {
+            this.addItem(this.state.Iname, this.state.Idescription);
+          }}
+        >
+          <Text style={{ color: "#fffff", fontSize: 18, fontWeight: "bold" }}>
+            Add Item
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: "75%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: "#ff5722",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+  },
+});
